571c4e9168b1a17dd6d0bd83c26bc7c3d52fdb2e
Maike Silva <maikesama@gmail.com>
2026-04-30 09:54:41 -0300

    Project created

---
+++ src/main/java/com/projecteme/testeme/TestemeApplication.java
@@ -0,0 +1,13 @@
+package com.projecteme.testeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projecteme/testeme/TestemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projecteme.testeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
